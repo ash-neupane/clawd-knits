2212a3321903221f332d1ec9cef46a0734702f95
ash-neupane <neupane.ashish@outlook.com>
2025-09-29 21:48:27 -0400

    Initial Commit

---
+++ clawd-knits/clawd_knitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  clawd_knitsApp.swift
+//  clawd-knits
+//
+//  Created by Ashish Neupane on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct clawd_knitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clawd-knitsTests/clawd_knitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  clawd_knitsTests.swift
+//  clawd-knitsTests
+//
+//  Created by Ashish Neupane on 9/29/25.
+//
+
+import Testing
+@testable import clawd_knits
+
+struct clawd_knitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
